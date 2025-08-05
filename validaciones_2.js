749dbcb9be16855840fd305d09721fd27aa35ce9
Sebastian Bustos <86892830+semati03@users.noreply.github.com>
2025-08-05 19:11:55 -0400

    Delete validaciones_2.js

--- validaciones_2.js
+++
@@ -1,9 +0,0 @@
-function validarComentario() {
-    // Validación del nombre
-    let nombreComentario = document.getElementById('nombreComentario');
-    if (nombreComentario.length < 3 && nombreComentario.length > 80) {
-        alert("El nombre debe tener entre 3 y 80 caracteres.");
-        return false;
-    }
-
-}
